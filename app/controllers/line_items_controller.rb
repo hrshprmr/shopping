087b2bfd2437a6ecaee5f6d4466cec79d1792394
hrshprmr <harsh.prmr1@gmail.com>
2020-12-21 13:30:49 +0530

    place order

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,6 +1,6 @@
 class LineItemsController < ApplicationController
-  before_action :create_order, only: [:create, :index]
-  # ss
+  before_action :create_order, only: [:index, :create, :destroy, :edit, :update]
+
   def index
     @line_items = @order.line_items
   end
@@ -10,6 +10,19 @@ class LineItemsController < ApplicationController
     @line_item.save
       redirect_to products_path
   end
+
+  def destroy
+    @line_item = @order.line_items.find(params[:id])
+    @line_item.destroy
+    redirect_to order_line_items_path
+  end
+
+
+  def update
+  @line_item = @order.line_items.find(params[:id])
+  @line_item.update(line_item_params)
+  redirect_to order_line_items_path
+end
 
   private
 
@@ -23,84 +36,8 @@ class LineItemsController < ApplicationController
       @order
     end
 
+
     def line_item_params
       params.require(:line_item).permit(:product_id, :quantity)
     end
-#   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
-#  before_action :set_product, only: [:create, :destroy]
-#   # GET /line_items
-#   # GET /line_items.json
-#   def index
-#     @line_items = LineItem.all
-#   end
-
-#   # GET /line_items/1
-#   # GET /line_items/1.json
-#   def show
-#   end
-
-#   # GET /line_items/new
-#   def new
-#     @line_item = LineItem.new
-#   end
-
-#   # GET /line_items/1/edit
-#   def edit
-#   end
-
-#   # POST /line_items
-#   # POST /line_items.json
-#   def create
-#     # @product = Product.find(params[:product_id])
-
-#     @line_item = @product.line_items.create(line_item_params)
-
-#     respond_to do |format|
-#       if @line_item.save
-#         format.html { redirect_to product_line_items_path, notice: 'Line item was successfully created.' }
-#         format.json { render :show, status: :created, location: @line_item }
-#       else
-#         format.html { render :new }
-#         format.json { render json: @line_item.errors, status: :unprocessable_entity }
-#       end
-#     end
-#   end
-
-#   # PATCH/PUT /line_items/1
-#   # PATCH/PUT /line_items/1.json
-#   def update
-#     respond_to do |format|
-#       if @line_item.update(line_item_params)
-#         format.html { redirect_to @line_item, notice: 'Line item was successfully updated.' }
-#         format.json { render :show, status: :ok, location: @line_item }
-#       else
-#         format.html { render :edit }
-#         format.json { render json: @line_item.errors, status: :unprocessable_entity }
-#       end
-#     end
-#   end
-
-#   # DELETE /line_items/1
-#   # DELETE /line_items/1.json
-#   def destroy
-#     @line_item.destroy
-#     respond_to do |format|
-#       format.html { redirect_to product_line_items_path, notice: 'Line item was successfully destroyed.' }
-#       format.json { head :no_content }
-#     end
-#   end
-
-#   private
-#   def set_product
-#     @product = Product.find(params[:product_id])
-#   end
-#     # Use callbacks to share common setup or constraints between actions.
-#     def set_line_item
-#       @line_item = LineItem.find(params[:id])
-#     end
-
-#     # Only allow a list of trusted parameters through.
-#     def line_item_params
-#       params.require(:line_item).permit(:product_id, :quantity, :price)
-#     end
-end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :orders do
     resources :line_items
   end
+  get '/payment' => 'orders#payment', :as => 'payment_option'
+  post 'orders/place_order'
   resources :products
   root 'products#index'
   get 'line_items/index'
